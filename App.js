fcda3f9740d5925c61e39596bbadad8cc3063001
Nikoolaii <nikolailemerre@gmail.com>
2022-08-02 16:10:41 +0200

    finish

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Button, Image, Text } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [img, setImg] = useState(" ");
+  const getCat = () => {
+    fetch("https://aws.random.cat/meow")
+      .then((response) => response.json())
+      .then((data) => {
+        setImg(data.file);
+        console.log(img);
+      });
+  };
+  useEffect(() => {
+    getCat();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Générateur de chat</Text>
+      <Image source={{ uri: img }} style={styles.img} />
+      <Button
+        onPress={getCat}
+        title="Générer une image"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: "80%",
+    height: "65%",
+    borderRadius: "15px",
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
